2bfab9d777b65b0f0f57e95d5be6e35a48ab70c3
redd <Benitez1504elmer@gmail.com>
2026-03-30 16:40:12 +0800

    First_Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Profile } from './assets/components/profile'
+
+function App() {
+    return (
+        <div className="App">
+            <Profile />
+        </div>
+    );
+}
+
+export default App
---
+++ src/assets/components/profile.jsx
@@ -0,0 +1,12 @@
+
+export const Profile = () => {
+    
+    return (
+        <div className="profile flex flex-col items-center justify-center h-screen  bg-gray-100">
+            <img src="cj.jpg" alt="Profile" />
+            <h1>Profile</h1>
+        </div>
+    )
+
+
+}
