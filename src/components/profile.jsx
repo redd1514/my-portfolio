bcb08b4c406035adf240776d3b4b9e434098766c
redd <Benitez1504elmer@gmail.com>
2026-04-05 02:10:15 +0800

    ewan

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,6 +2,8 @@ import profileImg from '../assets/cj.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import React, { useState, useEffect } from 'react';
 import { faSun } from '@fortawesome/free-solid-svg-icons';
 
@@ -20,7 +22,7 @@ const [theme, setTheme] = useState('dark');
         <div className="flex items-center justify-start py-5 w-full gap-6">
             <img src={profileImg} alt="Profile" className="w-[150px] h-[150px] mb-4 object-cover" />
             
-            <div className="flex flex-col items-start gap-2 w-full max-w-xl">
+            <div className="flex flex-col items-start gap-2 w-full max-w-2xl">
                 <div className="flex items-center justify-between gap-2 w-full">
                     <h1 className="text-3xl font-bold">Elmer Benitez II</h1>
                     <a>
@@ -35,9 +37,15 @@ const [theme, setTheme] = useState('dark');
                 
                 <h3 className="text-lg"> <FontAwesomeIcon icon={faLocationDot} /> Valenzuela City</h3>
                 <p>Pamantasan Ng Lungsod ng Valenzuela</p>
-                <div>
+                <div className="flex items-center gap-4 mt-2">
                 <a>
                     <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faEnvelope} /> Send Email</button>
+                </a>
+                <a href='https://github.com/redd1514'>
+                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faGithub} /> Github</button>
+                </a>
+                <a href='https://www.linkedin.com/in/elmer-benitez-867a46381/'>
+                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faLinkedin} /> Linkdin</button>
                 </a>
             </div>
             </div>
