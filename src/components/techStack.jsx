d389fdc05b403c40d8ffcc9150a6292e0b8bc68f
redd <Benitez1504elmer@gmail.com>
2026-04-05 02:20:35 +0800

    ewan ule

--- src/components/techStack.jsx
+++ src/components/techStack.jsx
@@ -2,8 +2,19 @@
 
 const TechStack = () => {
     return (
-        <div className="flex flex-col w-full bg-white dark:bg-[#0b0d0e] mt-2 p-3 px-5">
+        <div className="flex flex-col w-full bg-white dark:bg-[#0b0d0e] mt-2 p-3 px-5 gap-3">
             <h2><b>Tech Stack</b></h2>
+            <div className="space-y-4">
+                <div>
+                    <h3><b>Frontend</b></h3>
+                    <div className="flex flex-wrap gap-2 pt-2 ml-2">
+                        <span>JavaScript</span>
+                        <span>TypeScript</span>
+                        <span>React</span>
+                        <span>Tailwind CSS</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
