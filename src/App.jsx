8cdfd0b498d0c3a3ad902c68246ef61a1fb35202
redd <Benitez1504elmer@gmail.com>
2026-04-05 01:47:25 +0800

    wala lang

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Profile from './components/profile'
 import About from './components/About'
 import './index.css'
+import TechStack from './components/techStack'
 
 function App() {
     return (
@@ -9,9 +10,9 @@ function App() {
             <div className="w-full max-w-4xl flex flex-col ">
                 <Profile />
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-2">
-                    <div className="col-span-8">
+                    <div className="col-span-8">        
                         <About />
-                        
+                        <TechStack />
                     </div>
 
                 </div>
---
+++ src/components/techStack.jsx
@@ -0,0 +1,12 @@
+
+
+const TechStack = () => {
+    return (
+        <div className="flex flex-col w-full bg-white dark:bg-[#0b0d0e] mt-2 p-3 px-5">
+            <h2><b>Tech Stack</b></h2>
+        </div>
+    );
+
+}
+
+export default TechStack;
