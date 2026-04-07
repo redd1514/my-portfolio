9699f0ac9d3a1e365b579dd4c11f30edc6b07586
redd <Benitez1504elmer@gmail.com>
2026-04-07 21:23:00 +0800

    asdasd

--- src/components/techStack.jsx
+++ src/components/techStack.jsx
@@ -8,16 +8,7 @@ const TechStack = () => {
             </div>
                 
             <div className="space-y-4">
-                <div>
-                    <h3><b>Frontend</b></h3>
-                    <div className="flex flex-wrap gap-2 pt-2 ml-2">
-                        <span className="px-2 py-0.5 text-sm">JavaScript</span>
-                        <span className="px-2 py-0.5 text-sm">TypeScript</span>
-                        <span className="px-2 py-0.5 text-sm">React</span>
-                        <span className="px-2 py-0.5 text-sm">Tailwind CSS</span>
-                        <span className="px-2 py-0.5 text-sm">Vite</span>
-                    </div>
-                </div>
+                
                 <div>
                     <h3><b>Backend</b></h3>
                     <div className="flex flex-wrap gap-2 pt-2 ml-2">
