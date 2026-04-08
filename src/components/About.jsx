57fc3b9f9b637163c6453e611a8e3713a316deb1
redd <Benitez1504elmer@gmail.com>
2026-04-09 03:11:11 +0800

    initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                 I've helped startups and MSMEs grow and streamline their processes through software solutions. I've also built a community of over 200,000 developers sharing knowledge and mentorship.
                 <br />
                 <br />
-                Lately, I've been diving deeper into the world of artificial intelligence, focusing on integrating AI tools and techniques into modern applications. My work now includes developing AI-powered solutions, creating intelligent applications, and leveraging generative AI to optimize development workflows and deliver cutting-edge technology.    
+                Lately, I've been diving deeper into the world of fun intelligence, focusing on integrating AI tools and techniques into modern applications. My work now includes developing AI-powered solutions, creating intelligent applications, and leveraging generative AI to optimize development workflows and deliver cutting-edge technology.    
             </p>
         </div>
     )
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,14 +1,37 @@
-import profileImg from '../assets/cj.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import profileImg from '../assets/cj.jpg'
+import React, {useState, useEffect} from 'React'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const Profile = () => {
+    const [theme, setTheme] = useState('dark');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+    }
+
+    useEffect(() => {
+        document.documentElement.classList.toggle('dark', theme == 'dark');
+    }, [theme]);
+
     return (
         <div className="flex items-center justify-start py-5 w-full gap-6">
-            <img src="{profileImg}" alt="profile" className="w-[150px] h-[150px] mb-4 object-cover" />
+            <img src={profileImg} alt="Profile"  className="w-[159px] h-[159px] mb-4 object-cover"/>
             <div className="flex flex-col items-start gap-2 w-full max-w-2xl">
+                <div className="flex items-center justify-between gap-2 w-full">
+                    <a>
+                        <button
+                        className='py2'
+                        onClick={toggleTheme}
+                        >
+                        {theme === 'light' ? <FontAwesomeIcon icon={faMoon}/> : <FontAwesomeIcon icon={faSun}/>}
+                        </button>
 
+                    </a>
+                </div>
             </div>
+        
         </div>
-
     )
 
 }
