3e146d3a003cd5c645e868b77d173213b4f1c410
redd <Benitez1504elmer@gmail.com>
2026-04-08 13:56:10 +0800

    initial commit

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -18,7 +18,13 @@ const Experience = () => {
                         <span className="text-[10px] font-mono px-1.5 py-0.5">2026</span>
                     </div>
                 </div>
-                
+                <div className="relative pl-6 mt-4">
+                    <h3 className="text-sm font-semibold">Software Engineer</h3>
+                    <div className="flex items-center justify-between">
+                        <span className="text-xs">Cambridge</span>
+                        <span className="text-[10px] font-mono px-1.5 py-0.5">2026</span>
+                    </div>
+                </div>
 
             </div>
         </div>
