224e11b0ad6845fb3ab19c92ce5dfa4a4d6f1c10
redd <Benitez1504elmer@gmail.com>
2026-04-09 00:56:35 +0800

    Initialcommit

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,31 +1,42 @@
 const Experience = () => {
     return (
-        <div className="bg-white dark:bg-[#0b0d0e] p-5">
+        <div className="bg-white dark:bg-[#0b0d0e] p-5 space-y-2 group flex-1">
             <h2 className="text-lg font-bold">Experience</h2>
 
-            <div className="">
+            <div className="relative space-y-4 mt-4">
+                <div className="absolute left-1.5 top-1.5 bottom-2 w-px bg-gray-900"></div>
                 <div className="relative pl-6 mt-4">
+                    <div className="absolute left-0 top-1.5 w-3 h-3  border-2 border-gray-300 bg-gray-300"></div>
+                    <div className="space-y-1">
                     <h3 className="text-sm font-semibold">Software Engineer</h3>
                     <div className="flex items-center justify-between">
                         <span className="text-xs">Cambridge</span>
                         <span className="text-[10px] font-mono px-1.5 py-0.5">2026</span>
                     </div>
+                    </div>
                 </div>
                 <div className="relative pl-6 mt-4">
+                    <div className="absolute left-0 top-1.5 w-3 h-3  border-2 border-gray-300 bg-gray-300"></div>
                     <h3 className="text-sm font-semibold">AI Engineer</h3>
                     <div className="flex items-center justify-between">
-                        <span className="text-xs">Cambridge</span>
+                        <span className="text-xs">Coloong</span>
                         <span className="text-[10px] font-mono px-1.5 py-0.5">2026</span>
                     </div>
                 </div>
                 <div className="relative pl-6 mt-4">
                     <h3 className="text-sm font-semibold">Software Engineer</h3>
                     <div className="flex items-center justify-between">
-                        <span className="text-xs">Cambridge</span>
+                        <span className="text-xs">Tagalag</span>
                         <span className="text-[10px] font-mono px-1.5 py-0.5">2026</span>
                     </div>
                 </div>
-
+                <div className="relative pl-6 mt-4">
+                    <h3 className="text-sm font-semibold">Hello World!</h3>
+                    <div className="flex items-center justify-between">
+                        <span className="text-xs">My first line of code</span>
+                        <span className="text-[10px] font-mono px-1.5 py-0.5">2020</span>
+                    </div>
+                </div>
             </div>
         </div>
 
--- src/components/recentProject.jsx
+++ src/components/recentProject.jsx
@@ -11,22 +11,22 @@ const Recent = () => {
                 <div className="p-3 space-y-1">
                     <h3 className="text-sm font-semibold ">Heavenly cafe</h3>
                     <p className="text-xs ">Online Coffee Shop</p>
-                    <a href="https://heavenly-cafe.vercel.app/"><p className="text-xs px-2 py-1 font-mono bg bg-gray-100 dark:bg-black">heavenly-cafe.vercel.app</p></a>
+                    <a href="https://heavenly-cafe.vercel.app/"><p className="text-xs px-2 py-1 font-mono inline-block mt-1 rounded-md bg-gray-100 dark:bg-black">heavenly-cafe.vercel.app</p></a>
                 </div>
                  <div className="p-3 space-y-1">
                     <h3 className="text-sm font-semibold ">Valenzuela Surveying System</h3>
                     <p className="text-xs ">Online Survey System</p>
-                      <a href="https://valenzuela-survey.vercel.app/"><p className="text-xs px-2 py-1 font-mono bg bg-gray-100 dark:bg-black">valenzuela-survey.vercel.app/</p></a>
+                      <a href="https://valenzuela-survey.vercel.app/"><p className="text-xs px-2 py-1 font-mono inline-block mt-1 rounded-md bg-gray-100 dark:bg-black">valenzuela-survey.vercel.app/</p></a>
                 </div>
                  <div className="p-3 space-y-1">
                     <h3 className="text-sm font-semibold ">The Great Debate</h3>
                     <p className="text-xs ">Multiplayer Fighting Game</p>
-                      <a href="https://heavenly-cafe.vercel.app/"><p className="text-xs px-2 py-1 font-mono bg bg-gray-100 dark:bg-black">heavenly-cafe.vercel.app</p></a>
+                      <a href="https://heavenly-cafe.vercel.app/"><p className="text-xs px-2 py-1 font-mono inline-block mt-1 rounded-md bg-gray-100 dark:bg-black">heavenly-cafe.vercel.app</p></a>
                 </div>
                  <div className="p-3 space-y-1">
                     <h3 className="text-sm font-semibold ">Heavenly cafe</h3>
                     <p className="text-xs ">Online Coffee Shop</p>
-                      <a href="https://heavenly-cafe.vercel.app/"><p className="text-xs px-2 py-1 font-mono bg bg-gray-100 dark:bg-black">heavenly-cafe.vercel.app</p></a>
+                      <a href="https://heavenly-cafe.vercel.app/"><p className="text-xs px-2 py-1 font-mono binline-block mt-1 rounded-md bg-gray-100 dark:bg-black">heavenly-cafe.vercel.app</p></a>
                 </div>
             </div>
 
