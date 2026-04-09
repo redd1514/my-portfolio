d8e2dfcffc893950bea810b02764da332be269f4
redd <Benitez1504elmer@gmail.com>
2026-04-10 01:24:13 +0800

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,33 @@
-import profileImg from '../assets/cj.jpg';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import React, { useState, useEffect } from 'react';
-import { faSun } from '@fortawesome/free-solid-svg-icons';
+import About from './components/About'
+import './index.css'
+import TechStack from './components/techStack'
+import Recent from './components/recentProject'
+import Experience from './components/experience'
+import Certificate from './components/recentCertification'
+import Profile from './components/profile'
 
-const Profile = () => {
-const [theme, setTheme] = useState('dark');
-
-    const toggleTheme = () => {
-        setTheme(theme === 'dark' ? 'light' : 'dark');
-    };
-    
-    useEffect(() => {
-        document.documentElement.classList.toggle('dark', theme === 'dark');
-    }, [theme]);
-
+function App() {
     return (
-        <div className="flex items-center justify-start py-5 w-full gap-6">
-            <img src={profileImg} alt="Profile" className="w-[150px] h-[150px] mb-4 object-cover" />
-            
-            <div className="flex flex-col items-start gap-2 w-full max-w-2xl">
-                <div className="flex items-center justify-between gap-2 w-full">
-                    <h1 className="text-3xl font-bold">Elmer Benitez II</h1>
-                    <a>
-                    <button
-                        className="dark:text-white py-2"
-                        onClick={toggleTheme}
-                        >
-                        {theme === 'light' ?  <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
-                    </button>
-                    </a>
+        <div className="flex flex-col items-center min-h-screen px-4 pt-6">
+
+            <div className="w-full max-w-4xl flex flex-col ">
+                <Profile/>
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-2">
+                    <div className="col-span-8">        
+                        <About />
+                        <TechStack />
+                        <Recent />
+                    </div>
+                    <div className="col-span-4">
+                        <Experience />
+                    </div>
+                    <div className="col-span-1 md:col-span-6">
+                        <Certificate />
+                    </div>
                 </div>
-                
-                <h3 className="text-lg"> <FontAwesomeIcon icon={faLocationDot} /> Valenzuela City</h3>
-                <p>Pamantasan Ng Lungsod ng Valenzuela</p>
-                <div className="flex items-center gap-4 mt-2">
-                <a>
-                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faEnvelope} /> Send Email</button>
-                </a>
-                <a href='https://github.com/redd1514'>
-                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faGithub} /> Github</button>
-                </a>
-                <a href='https://www.linkedin.com/in/elmer-benitez-867a46381/'>
-                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faLinkedin} /> Linkdin</button>
-                </a>
             </div>
-            </div>
-            
         </div>
     )
+}
 
-
-};
-
-export default Profile;
+export default App
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,39 +1,59 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import profileImg from '../assets/cj.jpg'
-import React, {useState, useEffect} from 'react';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import profileImg from '../assets/cj.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import React, { useState, useEffect } from 'react';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
 
 const Profile = () => {
-    const [theme, setTheme] = useState('dark');
+const [theme, setTheme] = useState('dark');
 
     const toggleTheme = () => {
-        setTheme(theme === 'dark' ? 'light' : 'dark')
-    }
-
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
+    
     useEffect(() => {
-        document.documentElement.classList.toggle('dark', theme == 'dark');
+        document.documentElement.classList.toggle('dark', theme === 'dark');
     }, [theme]);
 
     return (
         <div className="flex items-center justify-start py-5 w-full gap-6">
-            <img src={profileImg} alt="Profile"  className="w-[159px] h-[159px] mb-4 object-cover"/>
+            <img src={profileImg} alt="Profile" className="w-[150px] h-[150px] mb-4 object-cover" />
+            
             <div className="flex flex-col items-start gap-2 w-full max-w-2xl">
                 <div className="flex items-center justify-between gap-2 w-full">
+                    <h1 className="text-3xl font-bold">Elmer Benitez II</h1>
                     <a>
-                        <button
-                        className='py2'
+                    <button
+                        className="dark:text-white py-2"
                         onClick={toggleTheme}
                         >
-                        {theme === 'light' ? <FontAwesomeIcon icon={faMoon}/> : <FontAwesomeIcon icon={faSun}/>}
-                        </button>
-
+                        {theme === 'light' ?  <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+                    </button>
                     </a>
                 </div>
+                
+                <h3 className="text-lg"> <FontAwesomeIcon icon={faLocationDot} /> Valenzuela City</h3>
+                <p>Pamantasan Ng Lungsod ng Valenzuela</p>
+                <div className="flex items-center gap-4 mt-2">
+                <a>
+                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faEnvelope} /> Send Email</button>
+                </a>
+                <a href='https://github.com/redd1514'>
+                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faGithub} /> Github</button>
+                </a>
+                <a href='https://www.linkedin.com/in/elmer-benitez-867a46381/'>
+                    <button className="dark:text-white py-2"> <FontAwesomeIcon icon={faLinkedin} /> Linkdin</button>
+                </a>
             </div>
-        
+            </div>
+            
         </div>
     )
 
-}
 
-export default Profile
+};
+
+export default Profile;
