54989358f90ff884f2be4c13206260658ec2f60d
redd <Benitez1504elmer@gmail.com>
2026-04-17 23:30:29 +0800

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Recent from './components/recentProject'
 import Experience from './components/experience'
 import Certificate from './components/recentCertification'
 import Profile from './components/profile'
+import Recommendations from './components/Recommendations'
 
 function App() {
     return (
@@ -12,17 +13,20 @@ function App() {
 
             <div className="w-full max-w-4xl flex flex-col ">
                 <Profile/>
-                <div className="grid grid-cols-1 lg:grid-cols-12 gap-2">
-                    <div className="col-span-8">        
+                <div className="grid grid-cols-1 lg:grid-cols-6 gap-2">
+                    <div className="col-span-4">        
                         <About />
                         <TechStack />
                         <Recent />
                     </div>
-                    <div className="col-span-4">
+                    <div className="col-span-2">
                         <Experience />
                     </div>
-                    <div className="col-span-1 md:col-span-6">
+                    <div className="col-span-1 md:col-span-3">
                         <Certificate />
+                    </div>
+                    <div className='col-span-1 md:col-span-3'>
+                        <Recommendations />
                     </div>
                 </div>
             </div>
