83d8689d39c7802bc27486d03920b6b45af6a7f0
redd <Benitez1504elmer@gmail.com>
2026-04-24 22:32:40 +0800

    initial

--- src/components/Recommendations.jsx
+++
@@ -1,9 +0,0 @@
-const Recommendations = () => {
-    return (
-        <div className="p-4 space-y-2  bg-white dark:bg-[#0b0d0e] ">
-            <h2 className="text-lg font-bold">Recommendations</h2>
-        </div>
-    )
-}
-
-export default Recommendations;
