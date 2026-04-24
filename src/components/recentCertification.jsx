be1a04aa833c5fbec35fc21a9b00bebd3d416e84
redd <Benitez1504elmer@gmail.com>
2026-04-24 22:31:31 +0800

    initial

--- src/components/recentCertification.jsx
+++ src/components/recentCertification.jsx
@@ -14,8 +14,8 @@ const Certificate = () => {
                 <h3 className="text-xs font-semibold">Machine Learning</h3>
                 <p className="text-[11px]">AI for Oceans</p>
             </a>
-            <a className="block p-2 dark:bg-black bg-gray-100 dark:hover:bg-gray-950 hover:bg-gray-300 transition-colors duration-200" href="https://drive.google.com/file/d/1s1YG399mEEYskXwcjVHEaAwnYjeC5PDD/view">
-                <h3 className="text-xs font-semibold">Frontend Development</h3>
+            <a className="block p-2 dark:bg-black bg-gray-100 dark:hover:bg-gray-950 hover:bg-gray-300 transition-colors duration-200" href="https://www.freecodecamp.org/certification/elmerbenitez/responsive-web-design">
+                <h3 className="text-xs font-semibold">Responsive Web Design V8</h3>
                 <p className="text-[11px]">FreeCodeCamp</p>
             </a>
             <a className="block p-2 dark:bg-black bg-gray-100 dark:hover:bg-gray-950 hover:bg-gray-300 transition-colors duration-200" href="https://drive.google.com/file/d/1s1YG399mEEYskXwcjVHEaAwnYjeC5PDD/view">
