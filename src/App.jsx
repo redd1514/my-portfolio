37b991d460ba7597efbf3f26e142bff826f98a1e
redd <Benitez1504elmer@gmail.com>
2026-04-29 22:58:35 +0800

    initially

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,7 @@ function App() {
                         <TechStack />
                         <Recent />
                     </div>
-                    <div className="col-span-2">
-                        <Experience />
-                    </div>
+                    
                     <div className="col-span-1 md:col-span-3">
                         <Certificate />
                     </div>
