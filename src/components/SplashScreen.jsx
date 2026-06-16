831d0090afc05b862ff80386fab97869477aafc9
redd <benitez1504elmer@gmail.com>
2026-06-17 00:29:24 +0800

    initial

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -32,7 +32,7 @@ const SplashScreen = ({ finishLoading }) => {
                     animation: 'luxuriousFade 2s cubic-bezier(0.4, 0, 0.2, 1) forwards'
                 }}
             >
-                Simple is better
+                Simple is 
             </h1>
             
             <style>{`
