eb0513e5a87b16f01e8d500d0a2afe388fce8b2c
redd <benitez1504elmer@gmail.com>
2026-07-08 16:47:52 +0800

    inital

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                 I love building software that connects smooth backend logic with clean, easy-to-use interfaces. My biggest project so far was serving as a Full-Stack Developer for our university department's official website. I helped build the platform from scratch to make it much easier for students and faculty members to find academic resources and navigate the site.
                 <br />
                 <br />
-                Right now, I'm focusing on making web . I'm learning to integrate AI models and frameworks like LangChain and other AI into traditional websites. My goal is to turn simple platforms into smart systems that can handle things like automatic scheduling, smart data search, and helpful, real-time AI assistants.
+                Right now, I'm focusing on making web applications. I'm learning to integrate AI models and frameworks like LangChain and other AI into traditional websites. My goal is to turn simple platforms into smart systems that can handle things like automatic scheduling, smart data search, and helpful, real-time AI assistants.
             </p>
         </div>
     )
